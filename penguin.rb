832355c498d8060f27f1726dff0716587fdd45f4
Isak Andersson <BitPuffin@lavabit.com>
2012-02-18 23:30:21 +0100

    added csrf protection to Penguin

--- penguin.rb
+++ penguin.rb
@@ -1,8 +1,23 @@
 Camping.goes :Penguin
 
+require 'camping'
+require 'camping/session'
+require 'rack/csrf'
+
 require_relative 'penguin/controllers'
 require_relative 'penguin/views'
 
 module Penguin
-	
+	use Rack::Csrf
+	include Camping::Session
+end
+
+module Penguin::Helpers
+	# Override form to always include the CSRF-tag
+	def form(*)
+		super do
+			self << Rack::Csrf.tag(@env)
+			yield
+		end
+	end
 end
